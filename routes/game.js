7ff6f54178202c7a526dbaedebe7834c3fddc55b
KimMinryoung <minryoung93@gmail.com>
2026-05-01 08:58:52 +0000

    fix: route legacy game links to nonogram

--- routes/game.js
+++ routes/game.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('public/game', {
-        pageTitle: '바벨 익스프레스',
-        pageDescription: 'Cyber-Lenin 사이트의 웹 게임 바벨 익스프레스입니다.',
-        pagePath: '/game',
-    });
+    res.redirect(301, '/nonogram/');
 });
 
 module.exports = router;
