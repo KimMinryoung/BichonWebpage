144680ad0e0fa6597ec288105ee7142c0f59a854
KimMinryoung <minryoung93@gmail.com>
2026-05-30 16:11:05 +0000

    feat: complete commulingo capital volume three

--- routes/commulingo.js
+++ routes/commulingo.js
@@ -8,7 +8,7 @@ const LESSONS_PATH = path.join(__dirname, '..', 'data', 'commulingo', 'lessons.j
 const PUBLIC_CHAPTER_LIMITS = {
     'capital-vol1': 33,
     'capital-vol2': 21,
-    'capital-vol3': 42,
+    'capital-vol3': 52,
 };
 
 function readLessons() {
